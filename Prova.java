edb480854c7c7e39bac4ac8599eb767e6e30a7ef
sunilsuraci <suracisunil@gmail.com>
2022-11-23 11:11:35 +0100

    Agg. Lettura

--- Prova.java
+++ Prova.java
@@ -6,9 +6,26 @@ import java.io.File;
 
 public class Prova {
     public static void main(String[] args){
-        
+      
+        try {
+            File file = new File("test.txt");
+            Scanner scan = new Scanner(file);
+
+            while (scan.hasNextLine()){
+                System.out.println(scan.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
        
     }
+
+
+    
+
+
+
+
 }
 
