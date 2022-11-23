9122181f84326b3c351c30081c2321fba38dd5dd
sunilsuraci <suracisunil@gmail.com>
2022-11-23 14:29:38 +0100

    inizioContenuto

---
+++ Contenuto.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Contenuto {
+    
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+        }
+
+
+    
+        public static boolean cerca(){
+            
+        }
+}
--- Prova.java
+++ Prova.java
@@ -4,11 +4,87 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
 
+import java.util.Scanner;
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class Prova {
     public static void main(String[] args){
       
+
+        
+        //ES 3
+
+    
+        
+        Random randomico = new Random();
+        int dimensione = randomico.nextInt(10)+1;
+        int[][] matrice = new int[dimensione][dimensione];
+ 
+        int somma=0;
+        for (int i = 0; i < dimensione; i++) {
+            for (int j = 0; j < dimensione; j++) {
+                int valore = randomico.nextInt(10);
+                somma+=valore;
+                matrice[i][j] = valore;
+            }
+        }
+        int media = somma/(dimensione*dimensione);
+ 
+        int[] maggioriMedia = new int[dimensione*dimensione];
+        int y=0;
+        for(int i=0; i<dimensione; i++){
+            for(int j=0; j<dimensione; j++){
+                if(matrice[i][j]>media){
+                    maggioriMedia[y]=matrice[i][j];
+                    y++;
+                }
+            }
+        }
+ 
+        int newDim;
+        double radice = Math.sqrt(dimensione);
+        if((radice%Math.round(radice))>0){
+            newDim = (int) Math.round(radice)+1;
+        }
+        else{
+            newDim = (int) Math.round(radice);
+        }
+        int[][] risultato = new int[newDim][newDim];
+ 
+        y=0;
+        for(int i=0; i<newDim; i++){
+            for(int j=0; j<newDim; j++){
+                
+                if(y<maggioriMedia.length){
+                    risultato[i][j]=maggioriMedia[y];
+                    y++;
+                }
+                else{
+                    risultato[i][j]=0;
+                }
+ 
+            }
+        }
+        
+        for(int i=0; i<newDim; i++){
+            for(int j=0; j<newDim; j++){
+                System.out.print(risultato[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+ 
+ 
+    
+
+       // ES. 1
+
         try {
             File file = new File("test.txt");
             Scanner scan = new Scanner(file);
@@ -33,6 +109,11 @@ public class Prova {
             System.out.println("Errore: " + errore.getMessage());
         }
     }
+
+
+    //ES 3
+
+    
 
 }
 
